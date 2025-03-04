932e8cfacb155af5619ac8b7f60e2f6aed3c20ac
George Guilherme <george.gui2000@gmail.com>
2025-03-04 17:39:16 -0300

    binary search

---
+++ BinarySearch.cs
@@ -0,0 +1,30 @@
+﻿namespace study_dsa;
+
+public static class BinarySearch
+{
+    public static int Handler(int[] arr, int n)
+    {
+        var leftPointer = 0;
+        var rigthPointer = arr.Length - 1;
+        var steps = 0;
+
+        while (leftPointer <= rigthPointer)
+        {
+            steps++;
+            var middlePosition = leftPointer + (rigthPointer - leftPointer) / 2;
+
+            if (arr[middlePosition] == n)
+            {
+                Console.WriteLine($"Encontado em {steps} tentativas.");
+                return middlePosition;
+            }
+            
+            if (arr[middlePosition] < n)
+                leftPointer = middlePosition + 1;                
+            else
+                rigthPointer = middlePosition - 1;
+        }
+
+        return -1;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,30 @@
 ﻿using study_dsa;
 
-string input1 = "Let's take LeetCode contest";
-string result1 = ReverseWords.Handler(input1);
-Console.WriteLine(result1); // Output: "s'teL ekat edoCteeL tsetnoc"
+#region Reverse Words
+
+// string input1 = "Let's take LeetCode contest";
+// string result1 = ReverseWords.Handler(input1);
+// Console.WriteLine(result1); // Output: "s'teL ekat edoCteeL tsetnoc"
+
+#endregion
+
+#region Binary Search
+int[] numbers = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 };
+int[] numbers2 = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39 };
+int[] numbers3 = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39,
+    41, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61, 63, 65, 67, 69, 71, 73, 75, 77, 79 };
+int[] numbers4 = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39,
+    41, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61, 63, 65, 67, 69, 71, 73, 75, 77, 79,
+    81, 83, 85, 87, 89, 91, 93, 95, 97, 99, 101, 103, 105, 107, 109, 111, 113, 115, 117, 119,
+    121, 123, 125, 127, 129, 131, 133, 135, 137, 139, 141, 143, 145, 147, 149, 151, 153, 155, 157, 159 };
+
+int target = 53;
+
+var result = BinarySearch.Handler(numbers4, target);
+Console.WriteLine(result);
+
+if (result != -1)
+    Console.WriteLine($"Valor {target} encontrado na posição {result}.");
+else
+    Console.WriteLine($"Valor {target} não encontrado no array.");
+#endregion
