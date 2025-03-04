ec656dd4c06b070c5bb1ac3a6804139eb36d6ff4
George Guilherme <george.gui2000@gmail.com>
2025-03-04 18:53:44 -0300

    sliding window

---
+++ SlidingWindow.cs
@@ -0,0 +1,33 @@
+﻿namespace study_dsa;
+
+public class SlidingWindow
+{
+    public static int Handler(string s)
+    {
+        if (string.IsNullOrEmpty(s))
+            return 0;
+        
+        int left = 0, right = 0;
+        int maxLenght = 1;
+        var dict = new Dictionary<char, int>();
+        dict.Add(s[0], 1); 
+
+        while (right < s.Length - 1)
+        {
+            right++;
+            if (dict.ContainsKey(s[right]))
+                dict[s[right]]++;
+            else
+                dict.Add(s[right], 1);
+
+            while (dict[s[right]] > 2)
+            {
+                dict[s[left]]--;
+                left++;
+            }
+            maxLenght = Math.Max(maxLenght, right - left + 1);
+        }
+
+        return maxLenght;
+    }
+}
