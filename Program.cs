a7322618e5c56a1d0188280a3fecfd3fce198f82
George Guilherme <george.gui2000@gmail.com>
2025-03-04 16:58:19 -0300

    reverse word with two pointers

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿using study_dsa;
+
+string input1 = "Let's take LeetCode contest";
+string result1 = ReverseWords.Handler(input1);
+Console.WriteLine(result1); // Output: "s'teL ekat edoCteeL tsetnoc"
---
+++ ReverseWords.cs
@@ -0,0 +1,32 @@
+﻿namespace study_dsa;
+
+public static class ReverseWords
+{
+    public static string Handler(string s)
+    {
+        var words = s.Split(' ');
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            words[i] = ReverseString(words[i]);
+        }
+
+        return string.Join(" ", words);
+    }
+
+    private static string ReverseString(string word)
+    {
+        char[] charArray = word.ToCharArray();
+        int left = 0, right = charArray.Length - 1;
+
+        while (left < right)
+        {
+            (charArray[left], charArray[right]) = (charArray[right], charArray[left]);
+
+            left++;
+            right--;
+        }
+
+        return new string(charArray);
+    }
+}
